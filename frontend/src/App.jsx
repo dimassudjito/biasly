afeab5831e00d47de4b4be3d368d750aa640c266
Dimas <almuhtad@ualberta.ca>
2023-09-16 09:04:26 -0400

    implement changing box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
+
 import './App.css'
-import {Header, TextInput, InfoCard} from './components'
+import {Header, TextInput, InfoCard, TextHighlight} from './components'
 
 function App() {
+  const [mode, setMode] = useState('write')
 
   return (
     <>
       <Header/>
+      <button 
+      className='bg-blue-500 hover:bg-blue-700 mb-4 text-white font-bold py-2 px-4 rounded'
+      onClick={() => setMode(mode == 'write' ? 'highlight' : 'write')}
+      >
+        {mode == 'write' ? 'Evaluate' : 'Edit'}
+      </button>
       <div className="grid grid-cols-2 gap-4">
-        <TextInput/>
+        {mode == 'write' ? <TextInput/> : <TextHighlight/>}
         <InfoCard/>
       </div>
     </>
