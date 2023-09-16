a067de8777153e11d650bff257339758de04e59d
Dimas <almuhtad@ualberta.ca>
2023-09-16 09:36:23 -0400

    POC for highlighting paragraph

--- frontend/src/components/TextHighlight.jsx
+++ frontend/src/components/TextHighlight.jsx
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types';
 
 function TextHighlight({content}) {
+    const fullText = content.split('.')
+    const highText = "Happy Birthday"
 
-        return (
-            <>
-                <h1 className="border-2 border-gray-300 rounded-md p-2">{content}</h1> 
-            </>
-        )
-    }
+    return (
+        <>
+            <p className="border-2 border-gray-300 rounded-md p-2">
+                {fullText.map(element => {
+                    if (element.includes(highText)) {
+                        return <span key={element} className='bg-blue-500'>{element}.</span>
+                    }
+                    return <span key={element}>{element}.</span>
+                })}
+            </p> 
+        </>
+    )
+}
 
 TextHighlight.propTypes = {
     content: PropTypes.string.isRequired,
