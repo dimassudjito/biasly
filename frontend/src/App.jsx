e6fde3a1f8d2debbd6c669eb92b397c665aceb55
Dimas <almuhtad@ualberta.ca>
2023-09-16 18:12:19 -0400

    add request to generate explanation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react'
 import './App.css'
 import {Header, TextInput, InfoCard, TextHighlight} from './components'
 import {CLASSIFY_REQUEST_BODY} from './data/classifierRequest'
+import { GENERATE_REQUEST_BODY } from './data/generatorRequest'
 
 const CLASSIFY_API = "https://api.cohere.ai/v1/classify"
+const GENERATE_API = "https://api.cohere.ai/v1/generate"
+const API_KEY = "kcLpZwryzepeisB5zpCSQtEVLeWdUVlGlUMFi2Db"
 
 function App() {
   const [mode, setMode] = useState('write')
@@ -23,12 +26,12 @@ function App() {
 
   const evaluateContent = async () => {
     setMode(mode == 'write' ? 'highlight' : 'write')
-    const inputs = content.split('.').filter((sentence) => sentence.length > 0)
+    const inputs = content.split('.').map(sentence => sentence.trim()).filter((sentence) => sentence.length > 0)
     CLASSIFY_REQUEST_BODY.inputs = inputs
     const response = await fetch(CLASSIFY_API, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer kcLpZwryzepeisB5zpCSQtEVLeWdUVlGlUMFi2Db`,
+        'Authorization': `Bearer ${API_KEY}`,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(CLASSIFY_REQUEST_BODY),
@@ -38,6 +41,25 @@ function App() {
       const obj = {text: classification.input, label: classification.prediction};
       return obj
     })
+    setFallacy(fallacies)
+    generateExplanation(fallacies)
+  }
+
+  const generateExplanation = async (fallacies) => {
+    for (var i = 0; i < fallacies.length; i++) {
+      if (fallacies[i].label == 'No Fallacy') continue
+      GENERATE_REQUEST_BODY.prompt = `Explain why the following sentence is a ${fallacies[i].label} logical fallacy: ${fallacies[i].text}`
+      const response = await fetch(GENERATE_API, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(GENERATE_REQUEST_BODY),
+      })
+      const body = await response.json()
+      fallacies[i].explanation = body.generations[0].text.trim()
+    }
     setFallacy(fallacies)
   }
 
---
+++ frontend/src/data/generatorRequest.js
@@ -0,0 +1,10 @@
+const GENERATE_REQUEST_BODY =  {
+    "model": "command",
+    "max_tokens": 200,
+    "temperature": 0.9,
+    "k": 0,
+    "stop_sequences": [],
+    "return_likelihoods": "NONE"    
+}
+
+export {GENERATE_REQUEST_BODY}
