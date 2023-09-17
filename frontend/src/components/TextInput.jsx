d6b0f6ca604b72d0665087421950ef83750f9eae
Dimas <almuhtad@ualberta.ca>
2023-09-16 20:04:08 -0400

    TextInput is styled now

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -5,10 +5,11 @@ function TextInput({content, editContent}) {
         return (
             <>
                 <textarea 
-                className="border-2 border-gray-300 rounded-md p-2" 
-                placeholder="Enter text here..."
+                className="border-black p-2 w-full h-screen bg-purple-300" 
+                placeholder="Enter article here..."
                 onChange={(e) => editContent(e.target.value)}
                 value={content}
+                style={{ borderWidth: '3px'}}
                 >
                 </textarea> 
             </>
