638c2a528cadd1f3c0b0d099b6f1651134b14706
Dimas <almuhtad@ualberta.ca>
2023-09-16 21:01:36 -0400

    style right column (minus shadow)

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 function InfoCard({ fallacyName, fallacyExplanation }) {
     return (
         <>
-            <div className="border-2 border-gray-300 rounded-md p-2">
-                <p>{fallacyName}</p>
+            <div className="border-black p-2 w-full h-screen bg-yellow-500 text-left h-fit" style={{ borderWidth: '3px', minHeight:"200px"}}>
+                <p className='font-custom text-2xl mb-4'>{fallacyName}</p>
                 <p>{fallacyExplanation}</p>
             </div>
         </>
